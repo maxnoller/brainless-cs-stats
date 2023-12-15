283de7a257ea900bd8f1620c84e66335fe720f9d
max <max.noller@asd.de>
2023-12-15 16:59:09 +0100

    changed env variables to be dynamic

--- rushb-stats-site/src/hooks.server.ts
+++ rushb-stats-site/src/hooks.server.ts
@@ -3,7 +3,7 @@ import GoogleProvider from '@auth/core/providers/google';
 import {
  GOOGLE_CLIENT_ID,
  GOOGLE_CLIENT_SECRET,
-} from '$env/static/private';
+} from '$env/dynamic/private';
 
 export const handle = SvelteKitAuth({
  providers: [GoogleProvider({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })]
