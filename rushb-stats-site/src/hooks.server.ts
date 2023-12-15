1ab47474ea88921905ec2696d455573e757da558
max <max.noller@asd.de>
2023-12-15 17:52:07 +0100

    added steam auth

--- rushb-stats-site/src/hooks.server.ts
+++ rushb-stats-site/src/hooks.server.ts
@@ -1,10 +1,17 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GoogleProvider from '@auth/core/providers/google';
+import SteamProvider from "next-auth-steam"
 import {
     env 
 } from '$env/dynamic/private';
 
 export const handle = SvelteKitAuth({
     trustHost: true,
- providers: [GoogleProvider({ clientId: env.GOOGLE_CLIENT_ID, clientSecret: env.GOOGLE_CLIENT_SECRET })]
+ providers: [
+     GoogleProvider({ clientId: env.GOOGLE_CLIENT_ID, clientSecret: env.GOOGLE_CLIENT_SECRET }),
+      SteamProvider(req, {
+        clientSecret: env.STEAM_SECRET!,
+        callbackUrl: 'https://cs-stats.labs.brainless.institute/auth/callback'
+      })
+ ]
 });
